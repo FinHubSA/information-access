5b0ca274b86f86f387a4a9743b9e15681fbb163f
Ryan Davies <mailforabe7@yahoo.com>
2021-09-12 23:16:24 +0200

    feat: added in router.js

---
+++ src/router.js
@@ -0,0 +1,23 @@
+import { createWebHistory, createRouter } from 'vue-router'
+import HelloWorld from './components/HelloWorld.vue'
+
+const routes = [
+  {
+    path: '/',
+    name: 'Home',
+    component: HelloWorld,
+  },
+  //Can add this in once search results is merged
+  //{
+  //path: '/search-results',
+  //name: 'Search Results',
+  //component: SearchResults,
+  //},
+]
+
+const router = createRouter({
+  history: createWebHistory(),
+  routes,
+})
+
+export default router
