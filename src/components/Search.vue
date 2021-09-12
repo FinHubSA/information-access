c45fdb37c1aed3bcf4cd7cc1de1114f9be5faace
Ryan Davies <mailforabe7@yahoo.com>
2021-09-12 23:02:38 +0200

    chore: removed border from results box

--- src/components/Search.vue
+++ src/components/Search.vue
@@ -1,37 +1,35 @@
 <template>
   <div class="hello">
     <h1>Search Results</h1>
-    <p>Short description of what you are looking at and possibly a summary bar.</p>
+    <p>
+      Short description of what you are looking at and possibly a summary bar.
+    </p>
   </div>
   <div class="flex-container">
-      <div class="vertical-flex-container">
-          <div class="option-select">
-                <div class="selected-option"> All time  </div>
-                <div > Since 2021  </div>
-                <div > Since 2020  </div>
-                <div > Since 2017  </div>
-                <div > Custom range  </div>
-          </div>
-
-          <div class="option-select">
-                <div class="selected-option"> Sort by journal  </div>
-                <div > Sort by date  </div>
-          </div>
-        
+    <div class="vertical-flex-container">
+      <div class="option-select">
+        <div class="selected-option">All time</div>
+        <div>Since 2021</div>
+        <div>Since 2020</div>
+        <div>Since 2017</div>
+        <div>Custom range</div>
       </div>
 
-
-      <div class="vertical-flex-container"> 
-          <div class="placeholder-result-boxes"> Result 1 </div>
-          <div class="placeholder-result-boxes"> Result 2 </div>
-          <div class="placeholder-result-boxes"> Result 3 </div> 
+      <div class="option-select">
+        <div class="selected-option">Sort by journal</div>
+        <div>Sort by date</div>
       </div>
+    </div>
+
+    <div class="vertical-flex-container">
+      <div class="placeholder-result-boxes">Result 1</div>
+      <div class="placeholder-result-boxes">Result 2</div>
+      <div class="placeholder-result-boxes">Result 3</div>
+    </div>
   </div>
 </template>
 
 <script>
-
-
 export default {
   name: 'Search',
   components: {
@@ -41,44 +39,41 @@ export default {
 </script>
 
 <style scoped>
-
 .flex-container {
-    display: flex;
-    padding: 1rem 5%;
-    justify-content: center;
-    align-items: center;
-    background-color: lightgrey;
-
+  display: flex;
+  padding: 1rem 5%;
+  justify-content: center;
+  align-items: center;
+  background-color: lightgrey;
 }
 
 .vertical-flex-container {
-    display: flex;
-    flex-direction: column;
-    padding: 1rem 5%;
-    
+  display: flex;
+  flex-direction: column;
+  padding: 1rem 5%;
 }
 
 .boxes {
-    border: 2px black;
-    border-style: solid;
+  border: 2px black;
+  border-style: solid;
 }
 
 .placeholder-result-boxes {
-    border: 2px black;
-    border-style: solid;
-    border-radius: 15px;
-    width: 400px;
-    height: 50px;
-    color: blue;
-    background-color: white;
-    padding: 1rem;
-    margin: 1rem;
+  border: 0px white;
+  border-style: solid;
+  border-radius: 15px;
+  width: 400px;
+  height: 50px;
+  color: blue;
+  background-color: white;
+  padding: 1rem;
+  margin: 1rem;
 }
 
 .option-select {
-    padding: 1rem;
+  padding: 1rem;
 }
 .selected-option {
-    color: darkorange;
+  color: darkorange;
 }
 </style>
