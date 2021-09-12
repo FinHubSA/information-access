e03b3977da0aa8394d5e80dd74dce2829e336ced
Ryan Davies <mailforabe7@yahoo.com>
2021-09-12 22:20:37 +0200

    chore: neatened up search results

--- src/components/Search.vue
+++ src/components/Search.vue
@@ -1,14 +1,36 @@
 <template>
   <div class="hello">
-    <h1>Welcome to Search!</h1>
-    <p>
-      A quick how to section over here. 
-    </p>
+    <h1>Search Results</h1>
+    <p>Short description of what you are looking at and possibly a summary bar.</p>
+  </div>
+  <div class="flex-container">
+      <div class="vertical-flex-container">
+          <div class="option-select">
+                <div class="selected-option"> All time  </div>
+                <div > Since 2021  </div>
+                <div > Since 2020  </div>
+                <div > Since 2017  </div>
+                <div > Custom range  </div>
+          </div>
+
+          <div class="option-select">
+                <div class="selected-option"> Sort by journal  </div>
+                <div > Sort by date  </div>
+          </div>
+        
+      </div>
+
+
+      <div class="vertical-flex-container"> 
+          <div class="placeholder-result-boxes"> Result 1 </div>
+          <div class="placeholder-result-boxes"> Result 2 </div>
+          <div class="placeholder-result-boxes"> Result 3 </div> 
+      </div>
   </div>
 </template>
 
 <script>
-//import SearchBar from './SearchBar.vue'
+
 
 export default {
   name: 'Search',
@@ -16,7 +38,47 @@ export default {
     //SearchBar,
   },
 }
-
 </script>
 
+<style scoped>
 
+.flex-container {
+    display: flex;
+    padding: 1rem 5%;
+    justify-content: center;
+    align-items: center;
+    background-color: lightgrey;
+
+}
+
+.vertical-flex-container {
+    display: flex;
+    flex-direction: column;
+    padding: 1rem 5%;
+    
+}
+
+.boxes {
+    border: 2px black;
+    border-style: solid;
+}
+
+.placeholder-result-boxes {
+    border: 2px black;
+    border-style: solid;
+    border-radius: 15px;
+    width: 400px;
+    height: 50px;
+    color: blue;
+    background-color: white;
+    padding: 1rem;
+    margin: 1rem;
+}
+
+.option-select {
+    padding: 1rem;
+}
+.selected-option {
+    color: darkorange;
+}
+</style>
