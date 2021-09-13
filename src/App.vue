d69e594c1b19e306805878eb4d618d4a4ae9a84b
RationalRyan11 <80746660+RationalRyan11@users.noreply.github.com>
2021-09-13 21:34:25 +0200

    feat: added router on new branch

--- src/App.vue
+++ src/App.vue
@@ -3,7 +3,7 @@
     <nav id="navbar">
       <h1>Placeholder for navbar</h1>
     </nav>
-    <home-page />
+    <router-view />
     <footer>
       <h1>Placeholder for footer</h1>
     </footer>
@@ -11,12 +11,10 @@
 </template>
 
 <script>
-import HomePage from './components/Home.vue'
 
 export default {
   name: 'app',
   components: {
-    HomePage,
   },
 }
 </script>
