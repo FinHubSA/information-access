a9d828c0e2be8e9ac52236f8f573ce84c1727e5f
Ryan Davies <mailforabe7@yahoo.com>
2021-09-13 08:50:33 +0200

    chore: changed main.js to be neater

--- src/main.js
+++ src/main.js
@@ -2,5 +2,5 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 
-createApp(App).mount('#app')
-App.use(router)
+const app = createApp(App).mount('#app')
+app.use(router)
