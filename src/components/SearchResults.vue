3399f436399f0436155a30a7d33b5eab46b382bc
RationalRyan11 <80746660+RationalRyan11@users.noreply.github.com>
2021-09-14 10:53:34 +0200

    chore: changed text below heading

--- src/components/SearchResults.vue
+++ src/components/SearchResults.vue
@@ -2,7 +2,7 @@
   <div>
     <h1>Search Results</h1>
     <p>
-      Short description of what you are looking at and possibly a summary bar.
+      Short description of what you are looking at, the search term, and possibly a summary bar. 
     </p>
   </div>
   <div class="flex-container">
