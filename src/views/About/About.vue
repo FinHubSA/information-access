886364f4bfdd8c24ca1f3bafc86e20dfaff100eb
Matt Johnson <matjohnson251@gmail.com>
2021-09-29 14:46:50 +0200

    feat: update about page information

--- src/views/About/About.vue
+++ src/views/About/About.vue
@@ -1,31 +1,39 @@
 <template>
   <div class="container">
-    <p>'''Brief history of Aaron Schwartz'''</p>
-    <br />
+    <h1>About</h1>
+    <h4>
+      <i>“Information is power. But like all power, there are those who want
+        to keep it for themselves”– Guerrilla Open Access Manifesto</i>
+    </h4>
     <p>
-      Our mission is to continue the work of Aaron Scwarts towards making
-      research accessible to all
+      Aaron Swartz was a computer programming prodigy and activist who played
+       an instrumental role in the campaign for a free and open Internet. 
+       In 2000, Swartz co-authored RSS version 1.0 and thereafter joined a 
+       working group at the World Wide Web Consortium to develop common data 
+       formats used on the World Wide Web- he was 14 at the time.  Swartz was 
+       also one of the early architects of the Creative Commons licences and a 
+       developer of Internet Archives’ Open Library – a free book database and digital 
+       library open to the public. He was an early leader in the development of Reddit, 
+       a giant online activist hub, as well as the co-founder of Demand Progress which was 
+       an effective advocacy group that led the United States Congress to back down from 
+       controversial antipiracy legislation. 
     </p>
     <p>
-      Too long have journal giants put articles and research that were
-      originally freely provided by academics behind a paywall restricting
-      information access to those that cannot afford it. The problem has become
-      so pervasive, that even the very institutions who sponsored the published
-      research is required to pay the fee to access the same article in these
-      journals.
+      Swartz was adamant that information, particularly that which may benefit society, be made freely available to the public. In 2011, Swartz liberated 4.8 million articles from an archive of academic journals called JSTOR which was available only by paid subscription to libraries and institutions. Federal prosecutors later charged him with two counts of wire fraud and eleven violations of the Computer Fraud and Abuse Act, carrying a cumulative maximum penalty of $1 million in fines, 35 years in prison, asset forfeiture, restitution, and supervised release. Swartz committed suicide on the 11 January 2013 while awaiting his trial.  Swartz was posthumously awarded the Madison Award for being an <i>“an outspoken advocate for public participation in government and unrestricted access to peer-reviewed scholarly articles.” </i>
+
     </p>
-    <p>
-      As it stands, regardless of whether you are a student at a university, an
-      independent researcher or some curious human being, because of paywalls
-      you can be hindered by being unable to access research that would further
-      your knowledge. We think this is unacceptable.
-    </p>
-    <p>Luckily, Academic research is always published as a pre-prints…</p>
+    <h3>The Vision of Aaron’s Toolkit - An Ecosystem Approach </h3>
+    <p>A young innovative advocate, an eloquent thinker; Aaron Swartz was a gift to the world. Aaron not only spoke about his ideas, but he pursued them vigorously. Aaron fought against the unfettered power of large institutions and ultimately ended his fight at 26 years old. However, the essence of Aaron’s fight has never ceased to exist as his vision continues to be pursued by a growing global community. Aaron’s Kit Team understands that Open Access is not about competition - instead, it’s about coordination and driving a community corporation. The key idea of Aaron’s Kit is accessibility at the highest order, and here we hope to democratise knowledge by freely providing access to academic journals.</p>
   </div>
 </template>
 <style scoped>
 .container {
+  display: flex;
   flex-grow: 1;
   flex-shrink: 0;
+  flex-direction: column;
+  justify-items: center;
+  align-items: center;
+   margin: 5px;
 }
 </style>
