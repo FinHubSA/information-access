85ae3ff42f81bde3103833dbe8fe9ed059d8cbf2
SJ Wu <wxxsij001@myuct.ac.za>
2021-10-10 18:05:49 +0200

    test: updating statusbar test for routing

--- src/components/SearchBar/StatusBar.test.js
+++ src/components/SearchBar/StatusBar.test.js
@@ -1,12 +1,13 @@
 import { shallowMount } from '@vue/test-utils'
 import StatusBar from './StatusBar.vue'
 import store from '../../services/DataService'
+import router from '../../router/index.js'
 
 describe('StatusBar.vue', () => {
   it('renders the page', () => {
     const wrapper = shallowMount(StatusBar, {
       global: {
-        plugins: [store],
+        plugins: [store, router],
       },
     })
     expect(wrapper.findComponent(StatusBar).exists()).toBe(true)
