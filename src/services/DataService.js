b8d0e741c39bac9d17381cbdcd7560c2894b9e1b
sjwu1235 <80747408+sjwu1235@users.noreply.github.com>
2021-10-12 14:53:17 +0200

    fix: Base URL fix (#78)
    * feat: updated baseURL in DataService.js to run off remote
    
    * feat: drawer closes on routing, sizing adjusted
    
    * refactor: removing unnecessary file from ResultCard folder
    
    * feat: added in routing to small home icon for mobile

--- src/services/DataService.js
+++ src/services/DataService.js
@@ -8,7 +8,7 @@ const Endings = {
 }
 
 const http = axios.create({
-  baseURL: 'http://api-aaronskit.org/api/',
+  baseURL: 'https://api-aaronskit.org/api/',
   headers: {
     'Content-type': 'application/json',
   },
